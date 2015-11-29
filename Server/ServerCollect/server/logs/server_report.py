ab25227cb4ffd8cbbb1050e5205d130c2735f555
felipe <felipe76857685@gmail.com>
2015-11-29 02:26:42 -0300

    Adicionadas algumas descrições

--- Server/ServerCollect/server/logs/server_report.py
+++ Server/ServerCollect/server/logs/server_report.py
@@ -9,11 +9,14 @@ from datetime import datetime
 import os
 from config import DEFAULT_LOG_DIR, DEFAULT_FILE_NAME_SERVER_REPORT
 
+LOCALE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+LOG_DIR = os.path.join(LOCALE_PATH, DEFAULT_LOG_DIR)
+
 def report(module, type, msg):
-    if not os.path.exists(DEFAULT_LOG_DIR):
-        os.makedirs(DEFAULT_LOG_DIR)
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
     
-    f = file(os.path.join(DEFAULT_LOG_DIR, DEFAULT_FILE_NAME_SERVER_REPORT), 'a')
+    f = file(os.path.join(LOG_DIR, DEFAULT_FILE_NAME_SERVER_REPORT), 'a')
     now = datetime.now()
     f.write(str(now) + " - "+module+" - "+type+" - "+msg+"\n")
     f.close()
