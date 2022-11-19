ea9d22bd083efee5beedecc503a6d53277d5390b
Henok Teixeira <henokteixeira@gmail.com>
2022-11-19 17:55:57 -0300

    update django.urls import

--- radarlivre_website/urls.py
+++ radarlivre_website/urls.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from django.conf.urls import url
+from django.urls import include, re_path
 from django.contrib.sitemaps.views import sitemap
 
 from radarlivre_website import views
@@ -11,17 +11,17 @@ sitemaps = {
 }
 
 urlpatterns = [
-    url(r'^$', views.index, name="page_default"),
-    url(r'^index/$', views.index, name="page_index"),
-    url(r'^downloads/$', views.downloads_index, name="page_downloads_index"),
-    url(r'^contrib/$', views.contrib, name="page_contrib"),
-    url(r'^about/$', views.about_index, name="page_about_index"),
-    url(r'^about/(?P<pk>[0-9]+)/$', views.about, name="page_about"),
+    re_path(r'^$', views.index, name="page_default"),
+    re_path(r'^index/$', views.index, name="page_index"),
+    re_path(r'^downloads/$', views.downloads_index, name="page_downloads_index"),
+    re_path(r'^contrib/$', views.contrib, name="page_contrib"),
+    re_path(r'^about/$', views.about_index, name="page_about_index"),
+    re_path(r'^about/(?P<pk>[0-9]+)/$', views.about, name="page_about"),
 
     # To help the search enguine
-    url(r'^googlec501605f6d7c9fb3.html$', views.googleSearchConsole,
+    re_path(r'^googlec501605f6d7c9fb3.html$', views.googleSearchConsole,
         name="google_search_console"),
 
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+    re_path(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
         name='django.contrib.sitemaps.views.sitemap')
 ]
