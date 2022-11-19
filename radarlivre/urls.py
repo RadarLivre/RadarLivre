2dac7c9f23cd669704ca299d492d8765c74a91db
Henok Teixeira <henokteixeira@gmail.com>
2022-11-19 16:26:52 -0300

    chore: update django.urls import

--- radarlivre/urls.py
+++ radarlivre/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url, include
+from django.urls import include, re_path
 from django.contrib import admin
 from radarlivre import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('radarlivre_api.urls')),
-    url(r'^', include('radarlivre_website.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^', include('radarlivre_api.urls')),
+    re_path(r'^', include('radarlivre_website.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
