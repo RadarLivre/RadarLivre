1d5002843a546a166c59d1cf4eda5a4a75cf6762
Henok Teixeira <henokteixeira@gmail.com>
2022-11-19 18:21:04 -0300

    chore: fix auto-created primary key warnings

--- radarlivre/settings.py
+++ radarlivre/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
