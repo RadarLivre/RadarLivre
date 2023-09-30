d7b4af1e6082f35b46d533bb71f73b2252be9c24
Henok Teixeira <henokteixeira@alu.ufc.br>
2023-09-30 15:16:01 -0300

    feat, fix: add a middleware to truncate filds with more than 10 decimal places

--- radarlivre/settings.py
+++ radarlivre/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'radarlivre_api.truncaction_middleware.RequestTruncationMiddleware'
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
---
+++ radarlivre_api/truncation_middleware.py
@@ -0,0 +1,36 @@
+import json
+
+class RequestTruncationMiddlewre:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        
+    def __call__(self, request):
+        if request.method == "POST":
+            json_data = json.loads(request.body)
+        
+            latitude, decimal_latitude = str(json_data[0]['latitude']).split('.')
+            longitude, decimal_longitude = str(json_data[0]['longitude']).split('.')
+            ground_tracking, decimal_ground_tracking = str(json_data[0]['groundTrackHeading']).split(".")
+            h_velocity, decimal_h_velocity = str(json_data[0]['horizontalVelocity']).split('.')
+                        
+            if len(decimal_latitude) > 10:
+                new_latitude = f"{latitude}.{decimal_latitude[:10]}"
+                json_data[0].update({'latitude': float(new_latitude)})
+                
+            if len(decimal_longitude) > 10:
+                new_longitude = f"{longitude}.{decimal_longitude[:10]}"
+                json_data[0].update({'longitude': float(new_longitude)})
+                
+            if len(decimal_ground_tracking) > 10:
+                new_ground_tracking = f"{ground_tracking}.{decimal_ground_tracking[:10]}"
+                json_data[0].update({'groundTrackHeading': float(new_ground_tracking)})    
+                
+            if len(decimal_h_velocity) > 10:
+                new_h_velocity = f"{h_velocity}.{decimal_h_velocity[:10]}"
+                json_data[0].update({'horizontalVelocity': float(new_h_velocity)})
+            
+            request._body = bytes(json.dumps(json_data), encoding='utf-8')
+        
+        response = self.get_response(request)
+        
+        return response
