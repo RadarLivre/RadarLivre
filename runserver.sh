c6939aa9f613f8c370d2bd9b7c0e83b3c7e231e3
Henok Teixeira <henokteixeira@gmail.com>
2025-01-09 09:39:11 -0300

    chore: update runserver.sh script

--- runserver.sh
+++ runserver.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 # The command above states wich shell is used
-BASEDIR=$(dirname "$0")
-cd "$BASEDIR"
+#BASEDIR=$(dirname "$0")
+#cd "$BASEDIR"
+#
+#echo "----- Activating virtual enviroment -----"
+#source venv/bin/activate
+#
+#echo "----- Running the server -----"
+#python3 manage.py runserver
+#
+#echo "----- Deactivating Virtual Enviroment -----"
+#deactivate
 
-echo "----- Activating virtual enviroment -----"
-source venv/bin/activate
-
-echo "----- Running the server -----"
-python3 manage.py runserver
-
-echo "----- Deactivating Virtual Enviroment -----"
-deactivate
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py runserver 0.0.0.0:8000
