316ee5fa777cb97db7f7fa0d54cd6f25e2101ed6
Henok Teixeira <henokteixeira@gmail.com>
2025-02-04 01:18:12 -0300

    refactor: fix pid_file variable use

--- performance_analysis/run_test_web_clients.sh
+++ performance_analysis/run_test_web_clients.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-CLIENT_COUNT=${1:-1}
 pid_file="web_clients_pids.txt"
 
-> "$PID_FILE"
+> "$pid_file"
 
 for i in $(seq 1 "$1"); do
     python3 web_client_simulator.py "client_$i" &
-    echo $! >> "$PID_FILE"
+    echo $! >> "$pid_file"
 
     sleep 15
 done
-
-echo "$CLIENT_COUNT clientes iniciados. PIDs salvos em $PID_FILE"
