65fb28f8ac738a259af6d12b28b618e7c6de19b5
Henok Teixeira <henokteixeira@gmail.com>
2025-02-16 16:31:17 -0300

    feat: add setting to hide logic from new model geographical field in serializers

--- radarlivre_api/serializers/__init__.py
+++ radarlivre_api/serializers/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
 
+qfrom decimal import Decimal
+
 from django.contrib.auth.models import User
+from django.contrib.gis.geos import Point
 from rest_framework import serializers
 from rest_framework.fields import ImageField
 from rest_framework.serializers import ModelSerializer
@@ -9,19 +12,37 @@ from radarlivre_api.models import Airport, Flight, Observation, \
     About, Notify, Collector, Airline, ADSBInfo, FlightInfo
 
 
+class SpatialCompatibleSerializer(serializers.ModelSerializer):  
+    def to_internal_value(self, data):
+        for field in ['latitude', 'longitude']:
+            if field in data and isinstance(data[field], str):
+                data[field] = Decimal(data[field])
+
+        if 'latitude' in data and 'longitude' in data:    
+            data['point'] = Point(
+                float(data['longitude']), 
+                float(data['latitude'])
+            )    
+
+        return super().to_internal_value(data)
+
+
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
 
 
-class CollectorSerializer(ModelSerializer):
+class CollectorSerializer(SpatialCompatibleSerializer):
     user = UserSerializer(read_only=True)
     key = serializers.UUIDField(format='hex_verbose', write_only=True)
 
     class Meta:
         model = Collector
         fields = '__all__'
+        extra_kwargs = {
+            'point': {'write_only': True}
+        }
 
 
 class AirlineSerializer(ModelSerializer):
@@ -30,10 +51,13 @@ class AirlineSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class ObservationSerializer(ModelSerializer):
+class ObservationSerializer(SpatialCompatibleSerializer):
     class Meta:
         model = Observation
         fields = '__all__'
+        extra_kwargs = {
+            'point': {'write_only': True}
+        }
 
 
 class FlightSerializer(ModelSerializer):
@@ -42,7 +66,7 @@ class FlightSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class FlightInfoSerializer(ModelSerializer):
+class FlightInfoSerializer(SpatialCompatibleSerializer):
     airline = AirlineSerializer(read_only=True)
     lastObservation = ObservationSerializer(read_only=True)
     flight = FlightSerializer(read_only=True)
@@ -50,18 +74,27 @@ class FlightInfoSerializer(ModelSerializer):
     class Meta:
         model = FlightInfo
         fields = '__all__'
+        extra_kwargs = {
+            'point': {'write_only': True}
+        }
 
 
-class AirportSerializer(ModelSerializer):
+class AirportSerializer(SpatialCompatibleSerializer):
     class Meta:
         model = Airport
         fields = '__all__'
+        extra_kwargs = {
+            'point': {'write_only': True}
+        }
 
 
-class ADSBInfoSerializer(ModelSerializer):
+class ADSBInfoSerializer(SpatialCompatibleSerializer):
     class Meta:
         model = ADSBInfo
         fields = '__all__'
+        extra_kwargs = {
+            'point': {'write_only': True}
+        }
 
 
 class AboutSerializer(ModelSerializer):
