103ff3b12a8ab12a7d6666907fbab0f2dd131c4f
Daniely M. Maia <daniely.maia@alu.ufc.br>
2025-12-04 12:16:29 -0300

    fix: correct activating virtual environment
    Fix comment formatting and activate virtual environment correctly.

--- runserver.sh
+++ runserver.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# The command above states wich shell is used
+# The command above states which shell is used
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
 echo "Activating virtual enviroment..."
-source .venv/bin/activate
+. .venv/bin/activate
 
 echo "Running the server..."
 python3 manage.py runserver 0.0.0.0:8000
